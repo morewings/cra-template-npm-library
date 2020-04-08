3135bcbbf02a97b3c58f417cd588d9a646f0ff0b
Dima Vyshniakov <morewings@gmail.com>
2020-04-09 00:43:47 +0200

    WIP

--- src/environment/App/App.js
+++ src/environment/App/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {Component, Counter} from 'lib';
+import {Counter} from 'lib';
 import classes from './App.module.css';
 
 const App = () => (
   <div className={classes.container}>
-    <Component />
     <Counter initialValue={0} />
   </div>
 );
