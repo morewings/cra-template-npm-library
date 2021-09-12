e887e0a7103c18e680716b2707d558bd40266c54
Dima Vyshniakov <morewings@gmail.com>
2021-09-12 12:51:07 +0200

    improve docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,6 +12,7 @@ module.exports = {
      * @example
      * ERROR in Error while loading rule 'prettier/prettier': context.getPhysicalFilename is not a function Occurred while linting /Users/dima/Dev/npm-test/src/lib/Counter/Counter.js
      *
+     * Another way of fixing is to delete `yarn.lock` and `node_modules` and run `yarn` again.
      */
     // "@storybook/preset-create-react-app"
   ]
