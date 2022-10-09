3273c7cd9ea4f9697da2e10deaa27ca5a5bc33eb
Dima Vyshniakov <morewings@gmail.com>
2022-10-09 22:08:07 +0200

    Fix incompabilities

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,16 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../docs/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  core: {
+    builder: 'webpack5',
+  },
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../docs/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app'
+  ],
+  staticDirs: ['../public'],
 }
